a8e555835aa63478d1d41d6bc0e99cf482a57687
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2022-12-24 23:45:10 +0100

    commit

---
+++ pkg/database/init.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"time"
+)
+
+// Init initializes the database
+func Init(db *sql.DB) error {
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	sqlStmt := fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (key TEXT PRIMARY KEY, url TEXT)", dbname)
+	_, err := db.ExecContext(ctx, sqlStmt)
+	if err != nil {
+		return err
+	}
+	return nil
+}
