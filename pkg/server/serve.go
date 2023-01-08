cdfb50bbfd559a70a9b238d66c381bbc98e4f8df
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2023-01-08 16:54:03 +0100

    commit

--- pkg/server/serve.go
+++ pkg/server/serve.go
@@ -26,5 +26,8 @@ func Serve(config env.Config, db *sql.DB) {
 	router.GET("favicon.ico", func(c *gin.Context) {
 		c.Status(404)
 	})
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
 	log.Fatal(router.Run(config.ListenHost + ":" + config.ListenPort))
 }
