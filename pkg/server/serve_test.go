2f3a4afb6280bb80a7252e7b63792f46d9864498
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2023-01-08 20:13:23 +0100

    commit

---
+++ pkg/server/serve_test.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"net/http"
+	"testing"
+)
+
+// TestServe is a test for the Serve function.
+func TestServe(t *testing.T) {
+	host := "https://urlshortener:8080/"
+	t.Run("RootTest", func(t *testing.T) {
+		resp, err := http.Get(host)
+		if err != nil {
+			t.Errorf("Error: %s", err.Error())
+		}
+		if resp.StatusCode != 200 {
+			t.Errorf("Expected status code to be 200, got %d", resp.StatusCode)
+		}
+	})
+}
