9b74a461e3dd354d9d7661d6bbc8635155f7b69b
Ruslan Gustomiasov <ruslan.gustomyasov@gmail.com>
2023-01-29 16:20:41 +0100

    commit

--- pkg/database/insertkey.go
+++ pkg/database/insertkey.go
@@ -2,14 +2,17 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"time"
+
+	"github.com/rusik69/urlshortener/pkg/env"
 )
 
 // InsertKey inserts the key url pair to database
 func InsertKey(key, value string) error {
 	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelfunc()
-	sqlStmt := "INSERT INTO shortener(key, url) VALUES($1, $2);"
+	sqlStmt := fmt.Sprintf("INSERT INTO %s(key, url) VALUES($1, $2);", env.ConfigInstance.DBTableName)
 	_, err := DB.ExecContext(ctx, sqlStmt, key, value)
 	if err != nil {
 		return err
